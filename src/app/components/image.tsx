267efdcd866b64be8e4495636588567e712a3594
StepanPotiienko <138608284+StepanPotiienko@users.noreply.github.com>
2024-07-28 15:34:04 +0300

    added eslint.json and prettierrc; added image component

---
+++ src/app/components/image.tsx
@@ -0,0 +1,9 @@
+const LayoutImage = () => {
+    return (
+        <div className="w-1/4 bg-red-600">
+            Image
+        </div>
+    )
+}
+
+export { LayoutImage };
---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import { LayoutImage } from "./components/image";
+
+export default function Home() {
+  return (
+    <div>
+      <LayoutImage />
+    </div>
+  );
+}
